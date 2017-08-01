4f8835843a3e3b38289235f97d0e7138e58b694b
Lam Tan <nhat.tan93@yahoo.com>
2017-08-01 15:21:59 +0200

    tp03-jsp

--- src/main/java/dev/sgp/web/EditerCollaborateurController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 public class EditerCollaborateurController extends HttpServlet {
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException{
 		
 		String matricule = req.getParameter("matricule");
 		String titre = req.getParameter("titre");
