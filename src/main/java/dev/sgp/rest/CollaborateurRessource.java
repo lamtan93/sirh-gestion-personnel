d0483fa9273cc4235205efd05d5745af798e197f
Lam Tan <nhat.tan93@yahoo.com>
2017-08-04 17:43:11 +0200

    Ressource Collab REST OK without tests

--- src/main/java/dev/sgp/rest/CollaborateurRessource.java
+++ src/main/java/dev/sgp/rest/CollaborateurRessource.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -13,6 +15,7 @@ import javax.ws.rs.core.MediaType;
 
 import dev.sgp.entite.Collaborateur;
 import dev.sgp.service.CollaborateurService;
+import dev.sgp.service.EditerCollaborateurService;
 
 @Path("/collaborateurs")
 public class CollaborateurRessource {
@@ -21,6 +24,9 @@ public class CollaborateurRessource {
 	@Inject
 	private CollaborateurService collabService;
 	
+	@EJB
+	@Inject
+	private EditerCollaborateurService editerService;
 	
 	List<Collaborateur> listCollab = new ArrayList<>();
 	
@@ -34,7 +40,7 @@ public class CollaborateurRessource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/collabByIdDepart/{id}")
+	@Path("/{id}")
 	public List<Collaborateur> listCollabByIdDepartement(@PathParam("id")int id){
 		
 		listCollab = collabService.getListCollabByIdDepart(id);
@@ -48,9 +54,47 @@ public class CollaborateurRessource {
 	public Collaborateur collabByMatricule(@PathParam("matricule")String matricule){
 		
 		Collaborateur collab = collabService.getCollabByMatricule(matricule);
-		
 		return collab;
 	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{matricule}")
+	public void editerCollaborateur(@PathParam("matricule")String matricule, Collaborateur collab){
+		
+		Collaborateur collabToModifie = editerService.findCollaborateurByMatricule(matricule);
+		editerService.editerCollaborateur(collabToModifie, collab.getIntitulePoste(), collab.getAdresse(), collab.getDepartement(), collab.getBanque(), collab.getBic(), collab.getIban());
+		
+	}
+	
+	
+	@GET
+	@Path("/{matricule}/banque")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getInfosBankByMatriculeCollab (@PathParam("matricule") String matricule){
+		
+		Collaborateur collab = editerService.findCollaborateurByMatricule(matricule);
+		
+		String infosBanque = collab.getBanque() + " " + collab.getBic()+ " " + collab.getIban();
+		
+		return infosBanque;
+}
+	
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/{matricule}/banque")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void modifieInfosBankByMatriculeCollab (@PathParam("matricule") String matricule, Collaborateur collabInput){
+		
+		Collaborateur collab = editerService.findCollaborateurByMatricule(matricule);
+		
+		editerService.editerCollaborateur(collab, collab.getIntitulePoste(), collab.getAdresse(), collab.getDepartement(), collabInput.getBanque(), collabInput.getBic(), collabInput.getIban());
+		
+		
+}
+	
 	
 	
 }
