785b55624cef8b38266a96c7a2130757e6a959cd
lamtan@ <lamtan93@gmail.com>
2017-08-08 08:30:18 +0200

    add CORS Filter

--- src/main/java/dev/sgp/rest/CollaborateurRessource.java
+++ src/main/java/dev/sgp/rest/CollaborateurRessource.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import dev.sgp.entite.Collaborateur;
 import dev.sgp.service.CollaborateurService;
@@ -35,6 +36,12 @@ public class CollaborateurRessource {
 	public List<Collaborateur> listCollabs (){
 		
 		 listCollab = collabService.getListeCollaborateurs();
+		 
+//		 return Response.status(200)
+//				 .entity(listCollab)
+//				 .header("Access-Control-Allow-Origin", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+//				 .build(); 
+		 
 		return listCollab;
 	}
 	
